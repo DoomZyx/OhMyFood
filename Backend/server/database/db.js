cf99131e4e72e3c7da1ca3404f39d487c7b08231
DoomZyx <axelcella.ac@gmail.com>
2025-01-19 22:56:20 +0100

    correction syntaxe menusTables et crétion de menus.db

--- Backend/server/database/db.js
+++ Backend/server/database/db.js
@@ -11,4 +11,14 @@ const db = new sqlite3.Database(dbPath, (err) => {
   }
 });
 
+// Définir le chemin absolu vers la base de données
+const dbm = new sqlite3.Database(path.join(__dirname, "../database/menus.db"), (err) => {
+  if (err) {
+    console.error("Erreur de connexion à la base de données :", err.message);
+  } else {
+    console.log("Connecté à la base de données SQLite.");
+  }
+});
+
 module.exports = db;
+module.exports = dbm;
