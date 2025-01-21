ed24f451f2ffa2cedcfd660de1289beb37d5957f
DoomZyx <axelcella.ac@gmail.com>
2025-01-21 20:37:53 +0100

    Ajout fleche retour

--- Frontend/src/Components/Header/header.jsx
+++ Frontend/src/Components/Header/header.jsx
@@ -1,11 +1,21 @@
 import logo from "../../../assets/logo/ohmyfood.png";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+
+  const hideArrowOnpaths = ["/"];
+
+
   return (
     <>
       <header>
+        {!hideArrowOnpaths.includes(location.pathname) && (
+          <Link to="/">
+            <i className="fa-solid fa-arrow-left"></i>
+          </Link>
+        )}
         <img src={logo} alt="logo" />
-
         <div className="location-search">
           <div className="location-icon"></div>
           <form>
