1f25677d09f6b681811a5ed186e92ab196b13a05
DoomZyx <axelcella.ac@gmail.com>
2025-02-20 10:46:59 +0100

    changement nom env

--- Backend/connection/connection.js
+++ Backend/connection/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config()
 
 mongoose
-  .connect(process.env.MONGOOSE_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
